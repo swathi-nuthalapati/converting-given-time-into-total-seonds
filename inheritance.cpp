72de5ce12f4e531ecfe8f7c299aa23598e974ea0
swathi <chittinuthalapati@gmail.com>
2020-12-30 07:09:38 -0800

    created inheritance.cpp
    print given time in total seconds

---
+++ inheritance.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <iomainp>
+
+usig namespace std;
+
+class Time
+{
+   private:
+        int seconds;
+        int hh,mm,ss;
+   public:
+        void getTime(void);
+        void convertIntoseconds(void)
+        void displayTime(void);
+};
+
+void Time::getTime(void)
+{
+    cout << "Enter time:" << endl;
+    cout << "Hours? ";
+    cin >> hh;
+    cout << "Minutes? ";
+    cin >> mm;
+    cout << "Seconds? ";
+    cin >> ss;
+}
+
+void Time::convertIntoSeconds(void)
+{
+     seconds = hh*3600 + mm*60 + ss;
+}
+
+void Time::displayTime(void)
+{
+     cout << "The time is = " << setw(2) << setfill('0') << hh << ":"
+                              << setw(2) << setfill('0') << mm << ":"
+                              << setw(2) << setfill('0') << ss << endl;
+     cout << "Time in total seconds: " << seconds;
+}
+
+int main()
+{
+   Time T;
+   
+   T.getTime();
+   T.convertIntoSeconds();
+   T.displayTime();
+   
+   return 0;
+   
+}
+        
